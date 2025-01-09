f06e1e9b2140034843c581e86ee7cc6abbfa9017
jarreedd <jarreedw@gmail.com>
2025-01-09 15:22:14 +0200

    add and delete task functionality

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+//DOM ELEMENTS
+const taskList = document.querySelector(".tasks-list");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
 
+// FUNCTIONS
+function formHandler(event) {
+    event.preventDefault();
+
+    // create task
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    taskList.appendChild(li);
+
+    // create task delete button
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    // clear text input
+    input.value = "";
+}
+
+function taskClickHandler(event) {
+    event.preventDefault();
+
+    if (event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+    }
+}
+
+//EVENT LISTENERS
+form.addEventListener("submit", formHandler);
+taskList.addEventListener("click", taskClickHandler);
