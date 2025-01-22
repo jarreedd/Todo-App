8e968304d9a6c1f7e38bb19d2855a0d5588c85cb
jarreedd <jarreedw@gmail.com>
2025-01-22 20:48:45 +0200

    save edited task

--- script.js
+++ script.js
@@ -11,21 +11,6 @@ function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function editTask(task) {
-    let index = task.getAttribute("data-index");
-}
-
-function deleteTask(task) {
-    // update state
-    let index = task.getAttribute("data-index");
-    tasks.splice(index, 1);
-
-    saveTasks();
-
-    // update ui
-    createTasks();
-}
-
 function createTask(task) {
     // create task
     const li = document.createElement("li");
@@ -62,7 +47,7 @@ function createTask(task) {
 
     // create edit task button
     const edit_btn = document.createElement("button");
-    edit_btn.className = "btn";
+    edit_btn.className = "icon-btn";
     edit_btn.classList.add("edit__btn");
     edit_btn.innerHTML = `
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -75,7 +60,7 @@ function createTask(task) {
 
     // create task delete button
     const delete_btn = document.createElement("button");
-    delete_btn.className = "btn";
+    delete_btn.className = "icon-btn";
     delete_btn.classList.add("delete__btn");
     delete_btn.innerHTML = `
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -93,9 +78,59 @@ function createTasks(tasks) {
     taskList.innerHTML = "";
 
     tasks.forEach((task) => {
-        const taskElement = createTask(task);
-        taskList.appendChild(taskElement);
+        taskList.appendChild(createTask(task));
     });
+}
+
+function editTask(task) {
+    let index = task.getAttribute("data-index");
+
+    const edit_text = document.createElement("input");
+    edit_text.type = "text";
+    edit_text.className = "edit__text";
+    edit_text.value = tasks[index].text;
+    edit_text.autocomplete = false;
+    edit_text.required = true;
+
+    const save_btn = document.createElement("button");
+    save_btn.className = "btn";
+    save_btn.classList.add("save__btn");
+    save_btn.innerText = "save";
+
+    const cancel_btn = document.createElement("button");
+    cancel_btn.className = "btn";
+    cancel_btn.classList.add("cancel__btn");
+    cancel_btn.innerText = "cancel";
+
+    task.classList.add("task__editing");
+    task.innerHTML = "";
+    task.appendChild(edit_text);
+    task.appendChild(save_btn);
+    task.appendChild(cancel_btn);
+}
+
+function saveEdit(task) {
+    const text = task.querySelector(".edit__text").value;
+    const index = task.getAttribute("data-index");
+    tasks[index].text = text;
+
+    saveTasks();
+    createTasks(tasks);
+}
+
+function deleteTask(task) {
+    // update state
+    let index = task.getAttribute("data-index");
+    tasks.splice(index, 1);
+
+    tasks.forEach((task, index) => {
+        task.index = index;
+        task.num = index + 1;
+    });
+    saveTasks();
+
+    // update ui
+    createTasks(tasks);
 }
 
 function formHandler(event) {
@@ -105,8 +140,8 @@ function formHandler(event) {
     const task = {
         text: input.value,
         isChecked: false,
-        index: tasks.length - 1,
-        num: tasks.length,
+        index: tasks.length,
+        num: tasks.length + 1,
     };
     tasks.push(task);
 
@@ -124,17 +159,25 @@ function taskClickHandler(event) {
     // event.preventDefault();
 
     if (event.target.tagName === "INPUT" || event.target.tagName === "LI") {
-        // update state
-        let index = event.target.getAttribute("data-index");
-        tasks[index].isChecked = tasks[index].isChecked ? false : true;
+        if (event.target.className == "edit__text") {
+            return;
+        } else {
+            let index = event.target.getAttribute("data-index");
+            tasks[index].isChecked = tasks[index].isChecked ? false : true;
 
-        saveTasks();
+            saveTasks();
+        }
     }
 
     if (event.target.tagName === "BUTTON") {
         let task = event.target.parentElement;
+
         if (event.target.classList.contains("edit__btn")) {
             editTask(task);
+        } else if (event.target.classList.contains("save__btn")) {
+            saveEdit(task);
+        } else if (event.target.classList.contains("cancel__btn")) {
+            console.log("cancel edit");
         } else if (event.target.classList.contains("delete__btn")) {
             deleteTask(task);
         }
