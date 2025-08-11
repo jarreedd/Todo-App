8c96bd958e5e18d16510b2b0d539555ac08e787d
jarreedd <jarreedw@gmail.com>
2025-08-11 18:36:08 +0200

    split up script

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-import { createTaskElement, createTasksElements, saveTasks } from "./task.js";
+import { createTaskElement, createTasksElements, saveTasks } from "./tasks.js";
+import { editTask } from "./task.js";
 
 // DOM ELEMENTS
 const taskList = document.querySelector(".tasks-list");
@@ -7,110 +8,83 @@ const input = document.querySelector("input");
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-function editTask(taskElement) {
-    let index = taskElement.getAttribute("data-index");
-
-    const edit_text = document.createElement("input");
-    edit_text.type = "text";
-    edit_text.className = "edit__text";
-    edit_text.value = tasks[index].text; // args for func should be task element and tasks array
-    edit_text.autocomplete = false;
-    edit_text.required = true;
-
-    const save_btn = document.createElement("button");
-    save_btn.className = "btn";
-    save_btn.classList.add("save__btn");
-    save_btn.innerText = "save";
-
-    const cancel_btn = document.createElement("button");
-    cancel_btn.className = "btn";
-    cancel_btn.classList.add("cancel__btn");
-    cancel_btn.innerText = "cancel";
-
-    taskElement.classList.add("task__editing");
-    taskElement.innerHTML = "";
-    taskElement.appendChild(edit_text);
-    taskElement.appendChild(save_btn);
-    taskElement.appendChild(cancel_btn);
-}
-
 function saveEdit(taskElement) {
-    const text = taskElement.querySelector(".edit__text").value;
-    const index = taskElement.getAttribute("data-index");
-    tasks[index].text = text;
+	const text = taskElement.querySelector(".edit__text").value;
+	const index = taskElement.getAttribute("data-index");
+	tasks[index].text = text;
 
-    saveTasks(tasks);
+	saveTasks(tasks);
 }
 
 function deleteTask(taskElement) {
-    // update state
-    let index = taskElement.getAttribute("data-index");
-    tasks.splice(index, 1);
+	// update state
+	let index = taskElement.getAttribute("data-index");
+	tasks.splice(index, 1);
 
-    tasks.forEach((task, index) => {
-        task.index = index;
-        task.num = index + 1;
-    });
-    saveTasks(tasks);
+	tasks.forEach((task, index) => {
+		task.index = index;
+		task.num = index + 1;
+	});
+	saveTasks(tasks);
 
-    // update ui
-    taskList.innerHTML = "";
-    taskList.appendChild(createTasksElements(tasks));
+	// update ui
+	taskList.innerHTML = "";
+	taskList.appendChild(createTasksElements(tasks));
 }
 
 function formHandler(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    // update state
-    const task = {
-        text: input.value,
-        isChecked: false,
-        index: tasks.length,
-        num: tasks.length + 1,
-    };
-    tasks.push(task);
+	// update state
+	const task = {
+		text: input.value,
+		isChecked: false,
+		index: tasks.length,
+		num: tasks.length + 1,
+	};
+	tasks.push(task);
 
-    saveTasks(tasks);
+	saveTasks(tasks);
 
-    // update ui
-    const taskElement = createTaskElement(task);
-    taskList.appendChild(taskElement);
+	// update ui
+	const taskElement = createTaskElement(task);
+	taskList.appendChild(taskElement);
 
-    // clear text input
-    input.value = "";
+	// clear text input
+	input.value = "";
 }
 
 function taskClickHandler(event) {
-    // event.preventDefault();
+	// event.preventDefault();
 
-    if (event.target.tagName === "INPUT" || event.target.tagName === "LI") {
-        if (event.target.className == "edit__text") {
-            return;
-        } else {
-            let index = event.target.getAttribute("data-index");
-            tasks[index].isChecked = tasks[index].isChecked ? false : true;
-        }
+	if (event.target.tagName === "INPUT" || event.target.tagName === "LI") {
+		if (event.target.className == "edit__text") {
+			return;
+		} else {
+			let index = event.target.getAttribute("data-index");
+			tasks[index].isChecked = tasks[index].isChecked ? false : true;
+		}
 
-        saveTasks(tasks);
-    }
+		saveTasks(tasks);
+	}
 
-    if (event.target.tagName === "BUTTON") {
-        let taskElement = event.target.parentElement;
+	if (event.target.tagName === "BUTTON") {
+		let taskElement = event.target.parentElement;
 
-        if (event.target.classList.contains("edit__btn")) {
-            editTask(taskElement);
-        } else if (event.target.classList.contains("save__btn")) {
-            saveEdit(taskElement);
+		if (event.target.classList.contains("edit__btn")) {
+			editTask(taskElement);
+		} else if (event.target.classList.contains("save__btn")) {
+			saveEdit(taskElement);
 
-            taskList.innerHTML = "";
-            taskList.appendChild(createTasksElements(tasks));
-        } else if (event.target.classList.contains("cancel__btn")) {
-            taskList.innerHTML = "";
-            taskList.appendChild(createTasksElements(tasks));
-        } else if (event.target.classList.contains("delete__btn")) {
-            deleteTask(taskElement);
-        }
-    }
+			taskList.innerHTML = "";
+			taskList.appendChild(createTasksElements(tasks));
+		} else if (event.target.classList.contains("cancel__btn")) {
+			taskList.innerHTML = "";
+			taskList.appendChild(createTasksElements(tasks));
+		} else if (event.target.classList.contains("delete__btn")) {
+			deleteTask(taskElement);
+		}
+	}
 }
 
 //EVENT LISTENERS
---
+++ src/tasks.js
@@ -0,0 +1,25 @@
+import { createTaskElement } from "./task";
+
+/**
+ * Creates a document fragment containing multiple task elements.
+ * @param {[]} tasks - An array of task objects to be rendered.
+ * @returns {DocumentFragment} A document fragment containing all task list item elements.
+ */
+export function createTasksElements(tasks) {
+	let fragment = document.createDocumentFragment();
+
+	tasks.forEach((task) => {
+		const taskElement = createTaskElement(task);
+		fragment.appendChild(taskElement);
+	});
+
+	return fragment;
+}
+
+/**
+ * Saves an array of tasks to localStorage.
+ * @param {[]} tasks - The array of task objects to save.
+ */
+export function saveTasks(tasks) {
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+}
